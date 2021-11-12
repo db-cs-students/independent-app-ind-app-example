dc002eab5c4363f1021c6cc1f8b475cf4b2f3656
Nathan Bailey <dev@dnbailey.org>
2021-11-12 11:53:27 -0500

    Initial Commit

---
+++ Independent Project/Independent_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Independent_ProjectApp.swift
+//  Independent Project
+//
+//  Created by Nathan Bailey on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Independent_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
